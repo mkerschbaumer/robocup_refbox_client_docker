f2e1d2487d2b9ae4088640851939ccb73f42b2a2
Martin Kerschbaumer <maff32@gmx.com>
2020-01-15 11:56:32 +0100

    Use different source for building refbox client Enables benchmark feedback

--- scripts/build_workspace.sh
+++ scripts/build_workspace.sh
@@ -4,9 +4,4 @@
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
 cd $ROS_WORKDIR &&
-    # only build refbox_comm
-    mv src/atwork_refbox_ros_client/ . &&
-    catkin_make &&
-    # Compile client
-    mv atwork_refbox_ros_client src/ &&
     catkin_make
